2ac5a616e75514040dcbee84d44950c7fe32b3c2
mercedeszuccolo <mechizuccolo@gmail.com>
2025-11-21 23:19:45 -0300

    testeamos requerimientos del maestro elemental, creamos la clase y la excepcion de falta de maestria

---
+++ src/ar/edu/unlam/pb2/parcial2/MaestroElemental.java
@@ -0,0 +1,53 @@
+package ar.edu.unlam.pb2.parcial2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MaestroElemental {
+	
+	private String nombre;
+	private Integer nivelDeMaestria;
+	private Afinidad afinidadElemental;
+	private Map<String, Criatura> criaturas;
+	
+	public MaestroElemental(String nombre, int nivelDeMaestria, Afinidad afinidadElemental) {
+		this.nombre = nombre;
+		this.nivelDeMaestria = nivelDeMaestria;
+		this.afinidadElemental = afinidadElemental;
+		this.criaturas = new HashMap<>();
+	}
+	
+	public void agregarCriatura(Criatura criatura) {
+		criaturas.put(criatura.getNombre(), criatura);
+
+	}
+	public Criatura getCriatura(String nombreCriatura) {
+		return criaturas.get(nombreCriatura);
+
+	}
+	
+	
+	 public void entrenar(String nombreCriatura) throws FaltaDeMaestriaException {
+	    Criatura criatura = criaturas.get(nombreCriatura);
+
+	    if (criatura == null) {
+	        return; 
+	        }
+
+	        if (this.nivelDeMaestria < 10) {
+	            throw new FaltaDeMaestriaException("La maestría es insuficiente.");
+	        }
+
+	        criatura.entrenar();
+	    }
+	 
+
+		public void pacificar(String nombreCriatura) {
+			Criatura criatura = criaturas.get(nombreCriatura);
+			if (criatura != null) {
+				criatura.pacificar();
+			}
+		}
+
+}
---
+++ test/ar/edu/unlam/pb2/parcial2/FaltaDeMaestriaException.java
@@ -0,0 +1,7 @@
+package ar.edu.unlam.pb2.parcial2;
+
+public class FaltaDeMaestriaException extends Exception {
+	public FaltaDeMaestriaException(String mensaje) {
+        super(mensaje);
+    }
+}
